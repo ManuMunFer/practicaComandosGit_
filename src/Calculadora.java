bae365020011b795e971a0f4a85be0ce513b3af7
manu <manumoriles3012@gmail.com>
2024-10-18 11:09:03 +0200

    Multiplicación

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -4,6 +4,9 @@ public class Calculadora {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Suma");
+		System.out.println("Resta");
+		System.out.println("Multiplicación");
+
 	}
 
 }
